35b784d28288062398f6fd9b4d7569691e4a40d5
ujjwal bhattarai <ujjwalbhattarai619@gmail.com>
2018-08-28 08:14:33 +0545

    change in file

---
+++ java-mvc-add-edit-delete/src/java/com/mvc/bean/AddBean.java
@@ -0,0 +1,19 @@
+package com.mvc.bean;
+
+public class AddBean {
+
+    private String name,owner;
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getOwner(){
+        return owner;
+    }
+    public void setOwner(String owner){
+        this.owner=owner;
+    }
+}
